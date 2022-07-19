d28aead5ab4fe9e6baf47b0d1412591a01487b76
hemanth bhushan <hemanthbhushan6@gmail.com>
2022-07-19 10:18:42 +0530

    init

---
+++ test/sample-test.js
@@ -0,0 +1,122 @@
+const { inputToConfig } = require("@ethereum-waffle/compiler");
+const { getSigners } = require("@nomiclabs/hardhat-ethers/dist/src/helpers");
+const { expect } = require("chai");
+const { ethers, waffle } = require("hardhat");
+const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
+const { getContractFactory } = require("hardhat/types");
+
+describe("test for pair ",()=>{
+    let Pair;
+    let pair;
+    let Factory;
+    let factory2;
+    let owner;
+    let feeToSetter1;
+    let signer1;
+    let signer2;
+    let tokenA;
+    let tokenB;
+    
+    beforeEach(async ()=>{
+        [owner,feeToSetter1,signer1,signer2,tokenA,tokenB] =await ethers.getSigners();
+        Factory = await ethers.getContractFactory("UniswapV2Factory");
+        factory2 = await Factory.deploy(feeToSetter1.address);
+        Pair = await ethers.getContractFactory("UniswapV2Pair",factory2.address);
+        pair = await Pair.deploy();
+        })
+    describe("checking constructor",()=>{
+        
+        it("check feeToSetter",async ()=>{
+            const fee = await factory2.feeToSetter();
+           expect(fee).to.be.equal(feeToSetter1.address);
+        })
+        it(" check factory address in pair",async ()=>{
+            const factory1 =  await pair.factory();
+            expect(factory1).to.equal(owner.address);
+
+        })
+
+    })
+     describe("check fee to",()=>{
+//         describe("if require condition sastified",()=>{
+//             it("check",async()=>{
+//                 await factory2.connect(feeToSetter1).setFeeTo(signer1.address);
+//                 expect(await factory2.feeTO()).to.equal(signer1.address);
+//  })
+        
+        // })
+        describe("if require condition not sastified",()=>{
+            it("check", ()=>{
+                    expect(factory2.setFeeTo(signer1.address)).to.be.revertedWith("UniswapV2: FORBIDDEN");
+            })
+        })
+    })
+    describe("check setFeeToSetter",()=>{
+        describe("if it sastifies the require",()=>{
+            it("check",async ()=>{
+                await factory2.connect(feeToSetter1).setFeeToSetter(signer1.address);
+                expect(await factory2.feeToSetter()).to.equal(signer1.address);
+
+            })
+        })
+        describe("if it is not sastifies the require",()=>{
+            it("check",async ()=>{
+                expect(factory2.setFeeToSetter(signer1.address)).to.be.revertedWith("UniswapV2: FORBIDDEN");
+
+            })
+        })
+
+    })
+    describe("check createPair",()=>{
+        describe("if the require condition sastified",()=>{
+            it("check",async()=>{
+              const createPair =   await factory2.createPair(tokenA.address,tokenB.address);
+              const createPair1 =   await factory2.createPair(signer1.address,signer2.address);
+            //   console.log("create pair",createPair);
+                const _Pair = await factory2.getPair(tokenA.address, tokenB.address);
+                const _pair1 = await factory2.getPair(tokenB.address,tokenA.address);
+
+                const pair_instance = await new UniswapV2Pair__factory(owner).attach(Pair);
+
+                const pairToken0 = await pair.token0();
+                const pairToken1 = await pair.token1();
+
+
+                // expect(pairToken0).to.equal(tokenA.address);
+                // expect(pairToken1).to.equal(tokenB.address);
+                
+
+                const length = await factory2.allPairsLength();
+
+                expect(_Pair).to.equal(_pair1);
+                expect(length).to.equal(2);
+
+
+            })
+
+        })
+        describe("if the require condition not sastified",()=>{
+            it("check",()=>{
+               // expect(factory2.createPair(tokenZero,tokenB.address)).to.be.revertedWith("UniswapV2: ZERO_ADDRESS");
+                expect(factory2.connect(owner).createPair(tokenA.address,tokenA.address)).to.be.revertedWith("UniswapV2: IDENTICAL_ADDRESSES");
+                expect(pair.initialize(tokenA.address,tokenB.address)).to.be.revertedWith('UniswapV2: FORBIDDEN');
+            })
+        })
+       
+     })
+     describe("length of the pairs ",()=>{
+        it("return length",async()=>{
+        const create = await factory2.connect(owner).createPair(tokenA.address,tokenB.address);
+        const create1 = await factory2.connect(owner).createPair(signer1.address,signer2.address);
+        
+        const length = await factory2.allPairsLength();
+        expect(length).to.equal(2);
+            
+        })
+    })
+   
+     
+    })
+   
+     
+
